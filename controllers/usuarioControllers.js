5b80c5d6ee0876fc7ba79c440d54014fc5642689
AxelUrizar <axelurizarmartin@gmail.com>
2023-01-07 21:01:30 +0100

    Creada función para loguear usuarios.
    El usuario introduciría su alias y contraseña y si pasa los factores de
    validación recibiría un token de autentificación con el que entrar a la
    página.

--- controllers/usuarioControllers.js
+++ controllers/usuarioControllers.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 const Usuario = require('../models/Usuarios')
+const Token = require('../models/Tokens')
 
 //
 // Devuelve una lista con todos los Usuarios en la base de datos.
@@ -14,6 +16,23 @@ exports.listaUsuarios = async (req, res) => {
         return res.status(200).json(listaUsuarios)
     } catch (e) {
        return res.status(500).json(e)
+    }
+}
+
+//
+// Devuelve la información del un Usuario.
+//
+exports.perfilUsuario = async(req, res) => {
+    try{
+        const { id } = req.params
+        if(!mongoose.Types.ObjectId.isValid(id)) return res.status(401).json('Id no válida.')
+
+        const perfilUsuario = await Usuario.findById(id)
+        if(perfilUsuario === null) return res.status(401).json('Usuario no encontrado.')
+
+        return res.status(200).json(perfilUsuario)
+    } catch(e){
+        return res.status(500).json(e)
     }
 }
 
@@ -37,18 +56,26 @@ exports.crearUsuario = async(req, res) => {
 }
 
 //
-// Devuelve la información del Usuario.
+// Loguea al usuario generando un token.
 //
-exports.perfilUsuario = async(req, res) => {
+exports.login = async(req, res) => {
     try{
-        const { id } = req.params
-        if(!mongoose.Types.ObjectId.isValid(id)) return res.status(401).json('Id no válida.')
+        const { alias, contraseña } = req.body
 
-        const perfilUsuario = await Usuario.findById(id)
-        if(perfilUsuario === null) return res.status(401).json('Usuario no encontrado.')
+        const usuario = await Usuario.findOne({ alias })
+        if(usuario === null) return res.status(401).json('El alias no coincide con ningún usuario.')
 
-        return res.status(200).json(perfilUsuario)
-    } catch(e){
+        const desencriptarContraseña = await bcrypt.compare(contraseña, usuario.contraseña)
+        if(!desencriptarContraseña) return res.status(401).json('Contraseña incorrecta.')
+
+        const generarToken = jwt.sign({ id: usuario._id }, process.env.JWT_SECRET)
+        await Token.create({
+            token: generarToken,
+            idUsuario: usuario._id
+        })
+
+        return res.status(200).json({ usuario: usuario, token: generarToken })
+    } catch(e) {
         return res.status(500).json(e)
     }
 }
