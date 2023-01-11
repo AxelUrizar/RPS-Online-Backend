ec3b541e60bce14bdb2c523359c4889b1aa46304
AxelUrizar <axelurizarmartin@gmail.com>
2023-01-11 13:25:46 +0100

    Cambiado nombre de variable en "controllers/usuarioControllers"
    Variable:
        - Usuario (antigua)
        - Usuarios (nueva)

--- controllers/usuarioControllers.js
+++ controllers/usuarioControllers.js
@@ -2,15 +2,15 @@ const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 
-const Usuario = require('../models/Usuarios')
-const Token = require('../models/Tokens')
+const Usuarios = require('../models/Usuarios')
+const Tokens = require('../models/Tokens')
 
 //
 // Devuelve una lista con todos los Usuarios en la base de datos.
 //
 exports.listaUsuarios = async (req, res) => {
     try {
-        const listaUsuarios = await Usuario.find({});
+        const listaUsuarios = await Usuarios.find({});
         if(listaUsuarios.length < 1) return res.status(200).json('No hay ningún usuario registrado.')
 
         return res.status(200).json(listaUsuarios)
@@ -27,7 +27,7 @@ exports.perfilUsuario = async(req, res) => {
         const { id } = req.params
         if(!mongoose.Types.ObjectId.isValid(id)) return res.status(401).json('Id no válida.')
 
-        const perfilUsuario = await Usuario.findById(id)
+        const perfilUsuario = await Usuarios.findById(id)
         if(perfilUsuario === null) return res.status(401).json('Usuario no encontrado.')
 
         return res.status(200).json(perfilUsuario)
@@ -47,14 +47,14 @@ exports.crearUsuario = async(req, res) => {
         if(!email) return res.status(401).json('Es necesario introducir un email.')
         if(contraseña.length < 5) return res.status(401).json('La contraseña debe contener almenos 5 carácteres.')
 
-        const comprobacionAlias = await Usuario.find({ alias: alias})
+        const comprobacionAlias = await Usuarios.find({ alias: alias})
         if(comprobacionAlias.length > 0) return res.status(401).json('El alias deseado ya está en uso.')
 
-        const comprobacionEmail = await Usuario.find({ email: email})
+        const comprobacionEmail = await Usuarios.find({ email: email})
         if(comprobacionEmail.length > 0) return res.status(401).json('El email deseado ya está en uso.')
 
         const contraseñaEncriptada = await bcrypt.hash(contraseña, 8)
-        const nuevoUsuario = await Usuario.create({ alias: alias, email: email, contraseña: contraseñaEncriptada })
+        const nuevoUsuario = await Usuarios.create({ alias: alias, email: email, contraseña: contraseñaEncriptada })
 
         return res.status(200).json(nuevoUsuario)
     } catch(e) {
@@ -69,14 +69,14 @@ exports.login = async(req, res) => {
     try{
         const { alias, contraseña } = req.body
 
-        const usuario = await Usuario.findOne({ alias })
+        const usuario = await Usuarios.findOne({ alias })
         if(usuario === null) return res.status(401).json('El alias no coincide con ningún usuario.')
 
         const desencriptarContraseña = await bcrypt.compare(contraseña, usuario.contraseña)
         if(!desencriptarContraseña) return res.status(401).json('Contraseña incorrecta.')
 
         const generarToken = jwt.sign({ id: usuario._id }, process.env.JWT_SECRET)
-        await Token.create({
+        await Tokens.create({
             token: generarToken,
             idUsuario: usuario._id
         })
@@ -92,7 +92,7 @@ exports.login = async(req, res) => {
 //
 exports.borrarUsuario = async(req, res) => {
     try {
-        const borrarUsuario = await Usuario.findByIdAndDelete(req.usuario._id)
+        const borrarUsuario = await Usuarios.findByIdAndDelete(req.usuario._id)
         console.log(borrarUsuario)
 
         return res.status(200).json('Usuario eliminado con éxito.')
